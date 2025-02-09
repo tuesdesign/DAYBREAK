e7731ef99ab013cd441876906583fe689a02cc02
Ace Savaid <69608587+AceSavaid@users.noreply.github.com>
2025-02-09 17:40:43 -0500

    Improved Lategame balancing
    (hopefully)

--- DAYBREAK/Assets/Scripts/Player/PlayerExpHandler.cs
+++ DAYBREAK/Assets/Scripts/Player/PlayerExpHandler.cs
@@ -49,8 +49,10 @@ public class PlayerExpHandler : MonoBehaviour
     {
         exp -= levelIncrement;
         level++;
-        levelIncrement += 10;
+        levelIncrement += 10 + ((int)(level/5)*2);
         
+
+
         // INSERT A CALL TO SPAWN THE UPGRADE MENU AND PAUSE THE TIME  (ALSO ENSURE THAT AFTER SELECTING THE UPGRADE MENU THAT TIME REVERTS)
         _upgradeManagerMenu.PopulateMenu();
 
