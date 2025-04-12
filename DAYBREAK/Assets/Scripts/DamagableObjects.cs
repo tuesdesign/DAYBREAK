7efdecf31fb69f64b4398d5c1a4fd4bfba6c2b6e
Ace Savaid <69608587+AceSavaid@users.noreply.github.com>
2025-04-12 02:47:01 -0400

    Level up bug fixes

--- DAYBREAK/Assets/Scripts/DamagableObjects.cs
+++ DAYBREAK/Assets/Scripts/DamagableObjects.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Audio;
 using UnityEngine.Serialization;
 
 public enum DamagableType
@@ -20,6 +21,22 @@ public class DamagableObjects : MonoBehaviour
     public void TakeDamage(float amount)
     {
         health -= amount;
+        switch (damagableType)
+            {
+                case DamagableType.Metal:
+                    SoundFXManager.Instance.PlaySoundFXClip(AudioClipManager.Instance.destructionMetalSound, transform, 1f);
+                    Debug.Log(damagableType);
+                    break;
+                case DamagableType.Wood:
+                    SoundFXManager.Instance.PlaySoundFXClip(AudioClipManager.Instance.destructionWoodSound, transform, 1f);
+                    Debug.Log(damagableType);
+                    break;
+                case DamagableType.Rock:
+                    Debug.Log(damagableType);
+                    break;
+                default:
+                    break;
+            }
         if (health <= 0)
         {
             DestroySelf();
